1e91512f23bbe73e6228cf7209746df314d75e54
Augusto V. B. Oliveira <augusto.vinicius137@gmail.com>
2021-09-18 15:11:55 -0300

    Ajustes na API KEY e na lógica do programa

--- app/src/main/java/br/com/etecia/localizausuarioapp/LocalizacaoActivity.java
+++ app/src/main/java/br/com/etecia/localizausuarioapp/LocalizacaoActivity.java
@@ -1,7 +1,6 @@
 package br.com.etecia.localizausuarioapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,27 +8,33 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class LocalizacaoActivity extends AppCompatActivity {
-    Button meu_botao;
-    EditText lat, longi;
-    int ilat, ilongi;
+    private Button m_botao;
+    private EditText m_campo_latitude, m_campo_longitude;
+    private double m_latitude, m_longitude;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_localizacao);
-        meu_botao =  findViewById(R.id.botao_local);
-        lat = findViewById(R.id.latitude);
-        longi = findViewById(R.id.longitude);
-        ilat = Integer.parseInt(lat.getText().toString());
-        ilongi = Integer.parseInt(longi.getText().toString());
 
-        meu_botao.setOnClickListener(new View.OnClickListener() {
+        m_botao = findViewById(R.id.botao_local);
+        m_campo_latitude = findViewById(R.id.latitude);
+        m_campo_longitude = findViewById(R.id.longitude);
+
+        m_botao.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), LocalizacaoActivity.class);
-                intent.putExtra("latitude",ilat);
-                intent.putExtra("longitude",ilongi);
+                m_latitude = Double.parseDouble(m_campo_latitude.getText().toString());
+                m_longitude = Double.parseDouble(m_campo_longitude.getText().toString());
+
+                Intent intent = new Intent(LocalizacaoActivity.this, MapsActivity.class);
+
+                intent.putExtra("m_valor_latitude", m_latitude);
+                intent.putExtra("m_valor_longitude", m_longitude);
+
                 startActivity(intent);
             }
         });
     }
+
 }
